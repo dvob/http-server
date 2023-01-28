3b2fe84c06c005e487fba1181aebdcbbd5a13be9
David Schneider <dsbrng25b@gmail.com>
2023-01-28 10:11:31 +0100

    Add request to info handler

--- handler.go
+++ handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
@@ -15,10 +16,15 @@ var handlers = map[string]http.HandlerFunc{
 
 func infoHandler(w http.ResponseWriter, r *http.Request) {
 	info := struct {
-		Hostname string `json:"hostname"`
+		Hostname string   `json:"hostname"`
+		Request  *request `json:"request"`
 	}{}
 	info.Hostname, _ = os.Hostname()
-	json.NewEncoder(w).Encode(info)
+	info.Request = newRequest(r)
+	err := json.NewEncoder(w).Encode(info)
+	if err != nil {
+		log.Println("failed to encode json:", err)
+	}
 }
 
 func okHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,4 +33,23 @@ func okHandler(w http.ResponseWriter, r *http.Request) {
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
 	io.Copy(w, r.Body)
+}
+
+type request struct {
+	Method     string      `json:"method"`
+	URI        string      `json:"uri"`
+	Protocol   string      `json:"protocol"`
+	Header     http.Header `json:"header"`
+	RemoteAddr string      `json:"remote_addr"`
+	// TLS evtl.
+}
+
+func newRequest(r *http.Request) *request {
+	return &request{
+		Method:     r.Method,
+		URI:        r.RequestURI,
+		Protocol:   r.Proto,
+		Header:     r.Header,
+		RemoteAddr: r.RemoteAddr,
+	}
 }
